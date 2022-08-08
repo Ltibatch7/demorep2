13f05aed653c08909533b3597ee31d2ace4676f4
Praveen Ram Kumar K <110518747+Ltibatch7@users.noreply.github.com>
2022-08-08 11:17:03 +0530

    update HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hi, Praveen");
+    System.out.println("Hi, Praveen. Today is 8th of August.");
   }
 }
